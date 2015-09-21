7e6f3393e22a7681eca6f4eb70555ca6ad626958
Chris Sells <csells@sellsbrothers.com>
2015-09-20 17:59:54 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,9 @@
 #gcloud preview app deploy $SRC_DIR/target/cloud-debugger-demo-v1/
 
 # deploy via GAE1
+#export PATH=$PATH:~/appengine-java-sdk-1.9.26/bin/
 echo check <application> in appengine-web.xml
 mvc clean install
-appcfg.sh update target/cloud-debugger-demo-v1
-appcfg.sh set_default_version target/cloud-debugger-demo-v1
+:~/appengine-java-sdk-1.9.26/bin/appcfg.sh update target/cloud-debugger-demo-v1
+:~/appengine-java-sdk-1.9.26/bin/appcfg.sh set_default_version target/cloud-debugger-demo-v1
 echo surf to https://csells-debugger-demo-1074.appspot.com/
